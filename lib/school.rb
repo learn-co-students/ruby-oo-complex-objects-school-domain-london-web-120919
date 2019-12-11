8d56bc61ddb38ddc99a75f566c45d245aca85629
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2019-12-11 15:55:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+    attr_reader :school, :student, :grade 
+    attr_accessor :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+      end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] <<  student
+    end 
+
+    def grade(student_grade)
+        roster.detect do |grade, student|
+            if grade == student_grade
+                return student
+            end
+        end
+    end 
+
+    def sort 
+        roster.each {|grade, student| student.sort!}
+    end 
+
+end 
